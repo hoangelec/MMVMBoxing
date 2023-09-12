7736e09de1579c40ca7f20209a2988524f780d28
Hoang Cap <hoangcap@Hoangs-MacBook-Pro.local>
2023-09-12 09:44:07 +0700

    Changed output mock file name for NetworkKit

--- LocalPods/NetworkKit/Tests/GeneratedMocks/NetworkKitMocks.swift
+++ LocalPods/NetworkKit/Tests/GeneratedMocks/NetworkKitMocks.swift
@@ -1,5 +1,6 @@
 // Generated using Sourcery 2.0.3 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
+
 // swiftlint:disable line_length
 // swiftlint:disable variable_name
 
@@ -12,16 +13,38 @@ import AppKit
 
 @testable import NetworkKit
 
+public class ImageLoaderMock: ImageLoader {
+
+    public init() {}
 
 
 
+    //MARK: - loadImage
 
+    public var loadImageFromThrowableError: Error?
+    public var loadImageFromCallsCount = 0
+    public var loadImageFromCalled: Bool {
+        return loadImageFromCallsCount > 0
+    }
+    public var loadImageFromReceivedUrl: (URL)?
+    public var loadImageFromReceivedInvocations: [(URL)] = []
+    public var loadImageFromReturnValue: UIImage!
+    public var loadImageFromClosure: ((URL) async throws -> UIImage)?
+    public func loadImage(from url: URL) async throws -> UIImage {
+        if let error = loadImageFromThrowableError {
+            throw error
+        }
+        loadImageFromCallsCount += 1
+        loadImageFromReceivedUrl = url
+        loadImageFromReceivedInvocations.append(url)
+        if let loadImageFromClosure = loadImageFromClosure {
+            return try await loadImageFromClosure(url)
+        } else {
+            return loadImageFromReturnValue
+        }
+    }
 
-
-
-
-
-
+}
 public class NetworkingSessionMock: NetworkingSession {
 
     public init() {}
@@ -39,7 +62,6 @@ public class NetworkingSessionMock: NetworkingSession {
     public var dataForReceivedInvocations: [(URLRequest)] = []
     public var dataForReturnValue: (Data, URLResponse)!
     public var dataForClosure: ((URLRequest) async throws -> (Data, URLResponse))?
-
     public func data(for request: URLRequest) async throws -> (Data, URLResponse) {
         if let error = dataForThrowableError {
             throw error
@@ -58,6 +80,9 @@ public class NetworkingSessionMock: NetworkingSession {
 public class RandomProtocolMock: RandomProtocol {
 
     public init() {}
+
+
+
     //MARK: - doSomething
 
     public var doSomethingWithCallsCount = 0
@@ -67,11 +92,11 @@ public class RandomProtocolMock: RandomProtocol {
     public var doSomethingWithReceivedArgument: (String)?
     public var doSomethingWithReceivedInvocations: [(String)] = []
     public var doSomethingWithClosure: ((String) -> Void)?
-
     public func doSomething(with argument: String) {
         doSomethingWithCallsCount += 1
         doSomethingWithReceivedArgument = argument
         doSomethingWithReceivedInvocations.append(argument)
         doSomethingWithClosure?(argument)
     }
+
 }
